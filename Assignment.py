4f8996ebcc77da6679376c056091b46315e096a8
chiragkarnwal <88793377+chiragkarnwal@users.noreply.github.com>
2022-02-05 21:26:29 +0530

    Day 6

---
+++ Assignment.py
@@ -0,0 +1,7 @@
+# #Take multiple inputs and print sum of the numbers...
+x=[int(i) for i in input("Enter the enumbers which you want to take the sum..")]
+sum=0
+for y in x:
+    sum+=y
+print("Here is the sum of the numbers",sum)    
+
